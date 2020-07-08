9da00410bfd1ec47faf90851b0bfc5e58df96d62
Matheus <mmedeiros450@gmail.com>
2020-07-08 11:19:33 -0300

    update code style

--- elf-parser.h
+++ elf-parser.h
@@ -5,36 +5,36 @@
 #include <elf.h>
 
 typedef struct {
-    Elf64_Shdr *header;
-    void *data;
-    size_t len;
+	Elf64_Shdr *header;
+	void *data;
+	size_t len;
 } section_t;
 
 typedef struct {
-    Elf64_Phdr *header;
-    void *data;
-    size_t len;
+	Elf64_Phdr *header;
+	void *data;
+	size_t len;
 } segment_t;
 
 typedef struct {
-    Elf64_Ehdr *header;
-    segment_t *segments;
-    size_t nsegments;
-    section_t *sections;
-    size_t nsections;
-    size_t nbytes;
+	Elf64_Ehdr *header;
+	segment_t *segments;
+	size_t nsegments;
+	section_t *sections;
+	size_t nsections;
+	size_t nbytes;
 } elf_t;
 
 typedef struct {
-    Elf64_Rela *rel;
-    Elf64_Sym *sym;
+	Elf64_Rela *rel;
+	Elf64_Sym *sym;
 } rela_t;
 
-section_t *getsectionbyname(elf_t *elf, const char *name);
-int getrelabyname(elf_t *elf, rela_t *rel, char *section_name, const char *name);
-Elf64_Sym *dynsym_name_lookup(elf_t *elf, const char *name);
-void elf_parser(elf_t *elf, const char *filename);
-void free_elf(elf_t *elf);
-
+section_t *getsectionbyname(elf_t * elf, const char *name);
+int getrelabyname(elf_t * elf, rela_t * rel, char *section_name,
+		  const char *name);
+Elf64_Sym *dynsym_name_lookup(elf_t * elf, const char *name);
+void elf_parser(elf_t * elf, const char *filename);
+void free_elf(elf_t * elf);
 
 #endif
